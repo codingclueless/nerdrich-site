d539d637f84f23d12b31489c8fea321d76a38161
codingclueless <codingclueless@gmail.com>
2020-02-28 15:11:33 -0800

    changed form submit to netlify

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,42 +1,44 @@
 import React, { useState } from 'react';
 import './Contact.scss';
 import Footer from '../footer/Footer';
-import axios from 'axios';
 import { useSpring, animated } from 'react-spring';
+
+const encode = (data) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+};
 
 const Contact = () => {
   const [name, setName] = useState('');
   const [subject, setSubject] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
-  const [setSent] = useState(false);
-  const [buttonText, setButtonText] = useState('Submit');
+  // const [setSent] = useState(false);
+  // const [buttonText, setButtonText] = useState('Submit');
 
-  const formSubmit = (e) => {
+  const handleSubmit = e => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', name, subject, email, message })
+    })
+      .then(() => alert('Success!'))
+      .catch(error => alert(error));
+
     e.preventDefault();
-    setButtonText('...sending');
-
-    let data = { name, subject, email, message };
-
-    axios.post('https://nerdrich-be.herokuapp.com/api/v1', data)
-      .then(res => {
-        setSent(true);
-        resetForm();
-        console.log('message has sent');
-      })
-      .catch(() => {
-        console.log('message not sent');
-      });
   };
 
+  
 
-  const resetForm = () => {
-    setName('');
-    setSubject('');
-    setEmail('');
-    setMessage('');
-    setButtonText('Message Sent');
-  };
+
+  // const resetForm = () => {
+  //   setName('');
+  //   setSubject('');
+  //   setEmail('');
+  //   setMessage('');
+  //   setButtonText('Message Sent');
+  // };
 
   const rightAnimated = useSpring({ from: { opacity: 0, transform: 'translateY(200px)' }, opacity: 1, transition: '1.5s ease-out', transform: 'translateY(0px)' });
   const leftAnimated = useSpring({ from: { opacity: 0, transform: 'translateY(-250px)' }, opacity: 1, transition: '1.5s ease-out', transform: 'translateY(0px)' });
@@ -50,24 +52,24 @@ const Contact = () => {
             <a className='sendIt' href="mailto:wesgriffincodes@gmail.com?Subject=send%20it" target="_top"> WesGriffinCodes@gmail.com</a>
           </p>
         </animated.div>
-        <animated.form style={rightAnimated} onSubmit={formSubmit} className='form'>
+        <animated.form style={rightAnimated} onSubmit={handleSubmit} className='form'>
           <div>
             <h3>Name</h3>
-            <input type="text" name='name' placeholder='enter your name' value={name} onChange={(e) => setName(e.target.value)} required/>
+            <input type="text" name="name" placeholder='enter your name' value={name} onChange={(e) => setName(e.target.value)} required/>
           </div>
           <div>
             <h3>Subject</h3>
-            <input type="text" name='subject' placeholder='enter your subject' value={subject} onChange={(e) => setSubject(e.target.value)} required/>
+            <input type="text" name="subject" placeholder='enter your subject' value={subject} onChange={(e) => setSubject(e.target.value)} required/>
           </div>
           <div>
             <h3>Email</h3>
-            <input type="email" name='email' placeholder='enter your email' value={email} onChange={(e) => setEmail(e.target.value)} required/>
+            <input type="email" name="email" placeholder='enter your email' value={email} onChange={(e) => setEmail(e.target.value)} required/>
           </div>
           <div>
             <h3>Message</h3>
-            <textarea className='message' placeholder='send me something' type="textarea" name='message' value={message} onChange={(e) => setMessage(e.target.value)} required/>
+            <textarea className="message" placeholder='send me something' type="textarea" name="message" value={message} onChange={(e) => setMessage(e.target.value)} required/>
           </div>
-          <button className='button' type='submit'><h3>{buttonText}</h3></button>
+          <button className='button' type="submit"><h3>Submit</h3></button>
         </animated.form>
       </div>
       <div className='footer'>
